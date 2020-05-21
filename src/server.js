028b4e8160dc2cb656b26455764140ac45f164b4
David G. Fonnegra <fonne@mattersupply.co>
2020-05-21 11:31:35 -0500

    Example

--- src/server.js
+++ src/server.js
@@ -5,12 +5,58 @@ const { gql } = require('apollo-server-lambda')
 const typeDefs = gql`
   type Query {
     hello: String
+    helloFromUser(user: String!): String
+    whoCanSendHello(domain: String): Users
+  }
+
+  type Users {
+    users: [User]
+  }
+
+  type User {
+    firstName: String
+    lastName: String
+    email: String
   }
 `
 
 const resolvers = {
   Query: {
-    hello: () => 'Hi! Love from @stemmlerjs 🤠.'
+    hello: () => 'Hi! Love from @fonne.',
+    helloFromUser: (parent, { user }) => `Hi! Love from ${user}`,
+    whoCanSendHello: (parent, { domain }) => {
+      if (domain === 'technologist') {
+        return {
+          users: [
+            {
+              firstName: 'Alejo',
+              lastName: 'Yarce',
+              email: 'alejo@mattersupply.co'
+            },
+            {
+              firstName: 'Miguel',
+              lastName: 'Santamaria',
+              email: 'miguelito@mattersupply.co'
+            },
+            {
+              firstName: 'Fabian',
+              lastName: 'Gomez',
+              email: 'fabian@mattersupply.co'
+            }
+          ]
+        }
+      }
+
+      return {
+        users: [
+          {
+            firstName: 'Kim',
+            lastName: 'Oakes',
+            email: 'kim@mattersupply.co'
+          }
+        ]
+      }
+    }
   }
 }
 
